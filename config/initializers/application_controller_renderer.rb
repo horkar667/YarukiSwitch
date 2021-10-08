73909585c9ca936d7f5ba1bb11c777890a22073f
horkar667 <jmmpp413@yahoo.co.jp>
2021-10-08 04:40:24 +0000

    [Update]自己紹介の追加など

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'ee7badaca830f90b0937ee12148023029ffcf1f0ddd8842497a7b65aa1b4e47434df1f625872f39838716681af0f5c7bbd084d2bf65e6dc45edd0d6ec0fe79ba'
