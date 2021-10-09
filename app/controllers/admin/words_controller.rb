97e90552a766df3ec3b58a1123d8806b9c4bef9c
horkar667 <jmmpp413@yahoo.co.jp>
2021-10-09 08:20:19 +0000

    [Add]admin側　ワード一覧画面　作成

--- app/controllers/admin/words_controller.rb
+++ app/controllers/admin/words_controller.rb
@@ -1,11 +1,15 @@
 class Admin::WordsController < ApplicationController
 
   def index
+    @words = Word.all
   end
 
   def show
   end
 
   def destroy
+    word = Word.find(params[:id])
+    word.destroy
+    redirect_to request.referrer || admin_root_path
   end
 end
