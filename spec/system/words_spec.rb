f1347b4d10b3e6291acfe79ddc528e989818d521
horkar667 <jmmpp413@yahoo.co.jp>
2021-10-13 05:23:20 +0000

    [Update]RSpecテスト

--- spec/system/words_spec.rb
+++ spec/system/words_spec.rb
@@ -3,7 +3,6 @@
 require 'rails_helper'
 
 describe '投稿のテスト' do
-  let!(:word) { create(:word, word:'頑張れ') }
   describe 'Word一覧画面のテスト' do
     before do
       visit words_path
@@ -11,6 +10,30 @@ describe '投稿のテスト' do
     context '表示の確認' do
       it 'words_pathが"/words"であるか' do
         expect(current_path).to eq('/words')
+      end
+      it '投稿されたものが表示されているか' do
+        (1..5).each do |i|
+	        Word.create(word:'hoge'+i.to_s)
+        end
+	      Word.all.each_with_index do |word|
+          expect(page).to have_content word.word
+          expect(page).to have_link word.word, href: word_path(word)
+        end
+      end
+    end
+  end
+
+  describe 'Word詳細画面のテスト' do
+	  let!(:word) { create(:word,word:'hoge') }
+    before do
+      visit word_path(word)
+    end
+    context '表示の確認' do
+      it 'word_path(word)が"/word/:id"であるか' do
+        expect(current_path).to eq("/words/#{word.id}")
+      end
+      it '投稿されたWordが表示されているか' do
+        expect(page).to have_content word.word
       end
     end
   end
