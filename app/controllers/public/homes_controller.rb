2024d3309184f383cc5b96825a817af644d58911
horkar667 <jmmpp413@yahoo.co.jp>
2021-10-19 02:00:49 +0000

    [Add]top画面のログイン状態の時の表示されるワードにUserがいいねしたものも表示されるようにした

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -2,8 +2,10 @@ class Public::HomesController < ApplicationController
 
   def top
     if current_user && current_user.words.count >= 1
-      @words = Word.where(user_id: current_user.id)
-      @random = @words.sample
+      favorites = Favorite.where(user_id: current_user).pluck(:word_id)
+      favorite_words = Word.find(favorites)
+      @words = Word.where(user_id: current_user)
+      @random = (favorite_words + @words).sample
     else
       @words = Word.all
       @random = @words.sample
