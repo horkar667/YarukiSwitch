9f823f2e93d5b45e006ec0e57c63a46357aafd27
horkar667 <jmmpp413@yahoo.co.jp>
2021-11-29 08:07:54 +0000

    [Update]インデント修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,13 +6,13 @@ class ApplicationController < ActionController::Base
 
   def ensure_current_user
     if current_user.id != params[:id].to_i
-     redirect_to user_path(current_user.id)
+      redirect_to user_path(current_user.id)
     end
   end
 
   def require_login
     unless current_user
-        redirect_to new_user_session_path, notice: 'Please sign in to get started!'
+      redirect_to new_user_session_path, notice: 'Please sign in to get started!'
     end
   end
 end
